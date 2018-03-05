82bfe95a19936fd2dfb40273dc76da8c256718c8
Anwar Montasir <anwarmontasir@gmail.com>
2018-03-05 15:08:33 -0800

    Note.js

---
+++ src/components/notes/Note.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { updateNote, removeNote } from './actions';
+import NoteForm from './NoteForm';
+
+class Note extends component {
+  state = {
+    editing: false;
+  };
+
+  handleEdit = note => {
+    this.props.updateNote(note);
+    this.setState({ editing: false });
+  }
+
+  handleToggleEdit = () => {
+    this.setState(prev => ({
+      editing: !prev.editing;
+    }));
+  };
+
+  render() {
+    // remember to use the injected prop for dispatch, 
+    // not the imported action creator
+    const { id, timestamp, text, removeNote } = this.props;
+    const { editing } = this.state;
+
+    return (
+      <li>
+        {editing ?
+          <NoteForm id={id} text={text} onEdit={this.handleEdit} /> :
+          <p><time>{timestamp.toLocaleString()}</time> - {text}</p>
+        }
+        <button onClick={this.handleToggleEdit}>
+         { editing ? 'cancel' : '✎'}
+        </button>
+        <button onClick={() => removeNote(id)}>X</button>
+      </li>
+    );
+  }
+}
+
+export default connect(
+  null,
+  { updateNote, removeNote }
+)(Note);
