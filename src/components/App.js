713fc664d3167cfb0b3146fcd2c98bbe526e975a
Anwar Montasir <anwarmontasir@gmail.com>
2018-03-05 14:55:15 -0800

    App.js

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import Notes from './notes/Notes';
+
+export default class App extends Component {
+
+  render() {
+    return (
+      <Notes />
+    );
+  }
+}
